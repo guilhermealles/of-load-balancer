99b5f549dca3e4d4d09a626361e39474ef07ab30
Guilherme Alles <guilherme.r.alles@gmail.com>
2017-06-28 00:21:55 -0300

    Adds timeout to the learning table

--- pox/HostProperties.py
+++ pox/HostProperties.py
@@ -1,11 +1,12 @@
 from pox.core import core
+import datetime
 
 class HostProperties (object):
-    
+
     def __init__(self):
         self.log = core.getLogger()
         self.reachableThroughPorts = []
-        self.knownIPs = []
+        self._knownIPsTimeout = {}
         self.lastMile = False
 
     def addUniquePort(self, port):
@@ -13,8 +14,16 @@ class HostProperties (object):
             self.reachableThroughPorts.append(port)
 
     def addUniqueIP(self, ipAddress):
-        if ipAddress not in self.knownIPs:
-            self.knownIPs.append(ipAddress)
+        self._updateIPsTimeout(ipAddress)
+        if ipAddress not in self._knownIPsTimeout:
+            self._knownIPsTimeout[ipAddress] = datetime.datetime.now()
 
     def isIPKnown(self, ipAddress):
-        return ipAddress in self.knownIPs
+        self._updateIPsTimeout(ipAddress)
+        return ipAddress in self._knownIPsTimeout
+
+    def _updateIPsTimeout(self, ipAddress):
+        if ipAddress in self._knownIPsTimeout:
+            now = datetime.datetime.now()
+            if (now - self.knownIPsTimeout[ipAddress]).seconds >= 1:
+                del self.knownIPsTimeout[ipAddress]
